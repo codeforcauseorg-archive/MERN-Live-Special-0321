98b1a35176ddbbf473981e3299e56d6b7db91fe6
KeenWarrior <anujgargcse@gmail.com>
2021-04-06 18:57:48 +0530

    l 4

--- lecture-04/askandshow.js
+++ lecture-04/askandshow.js
@@ -1,5 +1,6 @@
 let readlineSync = require('readline-sync');
 let axios = require("axios");
+const download = require('image-downloader');
 
 let username = readlineSync.question('What is your github username? : ');
 
@@ -9,9 +10,22 @@ let url = "https://api.github.com/users/" + username;
 axios.get(url).then(
     function(response){
         console.log(response.data.name);
+        
+        const options = {
+            url: response.data.avatar_url,
+            dest: './photos/'+username+".jpg"                // will be saved to /path/to/dest/image.jpg
+        }
+          
+        download.image(options);
+
     }
 ).catch(
     function(error){
         console.log(error);
     }
 )
+
+
+
+
+
