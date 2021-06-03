50a9026fea957799cdf10ebbe7b35aaf01c4e100
keenwarrior <anujgargcse@gmail.com>
2021-06-03 18:04:04 +0530

    l 29

---
+++ lecture-29/learnts/src/index.ts
@@ -0,0 +1,14 @@
+// function printName(id: number | string) {
+//     console.log(id);
+// }
+
+// printName("Anuj");
+
+type Point = {
+  x: number;
+  y?: number;
+};
+
+let some: Point = {
+    x : 88,
+}
